8f72e7893e4b555981a37c6591b8d0dcfa7c8d51
Abhishek Paradkar <aparadkar315@gmail.com>
2025-07-08 16:40:19 +0530

    Create a grid of 16 * 16

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 16; i++){
+    let div1 = document.createElement("div"); //create row of pixels
+    div1.setAttribute("style","display: flex; height: 56px; margin: 0; padding: 0;")
+    container.appendChild(div1);
+
+    for(let j = 0; j < 16; j++){
+    let div2 = document.createElement("div"); //create column of pixels
+    div2.setAttribute("style","display: flex; height: 56px; width: 56px; margin: 0; padding: 0; flex-shrink: 0;")
+    div1.appendChild(div2);
+    }
+}
+    
