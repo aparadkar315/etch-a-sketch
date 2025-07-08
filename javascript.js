a20b6e12a3aca4628fd656bd042ccd10b370a1e0
Abhishek Paradkar <aparadkar315@gmail.com>
2025-07-08 22:19:48 +0530

    Set up a hover effect, add margin to container

--- javascript.js
+++ javascript.js
@@ -7,8 +7,15 @@ for(let i = 0; i < 16; i++){
 
     for(let j = 0; j < 16; j++){
     let div2 = document.createElement("div"); //create column of pixels
-    div2.setAttribute("style","display: flex; height: 56px; width: 56px; margin: 0; padding: 0; flex-shrink: 0;")
+    div2.setAttribute("style","display: flex; height: 56px; width: 56px; margin: 0; padding: 0; flex-shrink: 0;");
+    div2.classList.toggle("pixel");
     div1.appendChild(div2);
     }
 }
-    
+
+const pixels = document.querySelectorAll(".pixel");
+pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseenter",() => {
+    pixel.style.background = "blue";
+    });
+});
